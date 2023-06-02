d50835ff3591c75b0755604a51eca4d30018b0d9
Christoper0704 <c.luis0704@gmail.com>
2023-06-02 12:10:34 +0700

    Login Register Backend

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const Role = db.role;
 db.sequelize.sync();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to MyApp" });
 });
 
 require('./app/routes/auth.routes')(app);
@@ -29,20 +29,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
-  });
- 
-  Role.create({
-    id: 2,
-    name: "moderator"
-  });
- 
-  Role.create({
-    id: 3,
-    name: "admin"
-  });
-}
